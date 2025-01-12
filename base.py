28e2abe8cb4edffc3ac5139ca5eef156d5cd3916
epelayoc <97033678+epelayoc@users.noreply.github.com>
2025-01-12 19:07:12 +0100

    Update base.py

--- base.py
+++ base.py
@@ -36,7 +36,7 @@ def generate_summary(document_data):
 
 st.title("Conversational Assistant")
 
-pdf_url = 'https://www.cdti.es/sites/default/files/2024-11/manual_complementario_especifico_sneo_v.4.0.pdf'
+pdf_url = 'https://www.cdti.es/ayudas/ayudas-neotec-2024'
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -44,23 +44,24 @@ if "messages" not in st.session_state:
 if pdf_url:
     document_data = load_document(pdf_url)
     if document_data:
-        with st.spinner("Generando resumen inicial..."):
-             summary = generate_summary(document_data)
-        st.subheader("Manual Neotec")
-        st.write(summary)
+        #with st.spinner("Generando resumen inicial..."):
+        #     summary = generate_summary(document_data)
+        st.subheader("Ficha Neotec 2024")
+        st.write('El programa NEOTEC 2024 NEOTEC 2024 apoya empresas de base tecnológica (EBT) con subvenciones de hasta el 70% (o 85% con doctores), con un máximo de 250.000€ (o 325.000€ con doctores), para proyectos basados en desarrollo tecnológico propio e I+D+i. Dirigido a pequeñas empresas innovadoras de hasta 3 años, con un capital mínimo de 20.000€ y un presupuesto mínimo de proyecto de 175.000€. Prioriza proyectos liderados por mujeres y financia gastos como personal, equipos y consultoría. La convocatoria fue del 10 de abril al 10 de mayo de 2024.
+')
         st.write('Puedes interactúar con el documento haciéndole preguntas.')
 
         for message in st.session_state.messages:
               with st.chat_message(message["role"]):
                   st.write(message["content"])
 
-        if prompt := st.chat_input("Ask a question about the document"):
+        if prompt := st.chat_input("Pregunta al documento"):
           st.session_state.messages.append({"role": "user", "content": prompt})
           with st.chat_message("user"):
                 st.write(prompt)
 
           with st.chat_message("assistant"):
-                with st.spinner("Thinking..."):
+                with st.spinner("Pensando..."):
                     response = generate_response(document_data, prompt)
                     st.write(response)
           st.session_state.messages.append({"role": "assistant", "content": response})
